ab0b2aa06ae206e081004a6c37e1f7f198ea5d87
laurent <wrmultitudes@gmail.com>
2020-11-29 10:24:19 +0100

    adding arg parser

--- Sources/Day1/main.swift
+++ Sources/Day1/main.swift
@@ -1,1 +1,21 @@
-print("Hello, world!")
+import ArgumentParser
+
+// Define our parser.
+struct Day1: ParsableCommand {
+  // Declare expected launch argument(s).
+  @Option(help: "Specify an Integer.")
+  var input: Int
+
+  func run() throws {
+	print("Running Day1 Challenge with input \(input)")
+  }
+}
+
+// Run the parser.
+Hello.main()
+
+
+
+
+
+//print("Hello, world!")
