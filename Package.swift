201aac1bbd482d77a7709559b20d52f20d635fef
laurent <wrmultitudes@gmail.com>
2020-12-04 17:49:32 +0100

    day4

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "AdventOfCode2020",
     dependencies: [
 		.package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.3.0"),
-		.package(url: "https://github.com/multitudes/AdventKit.git", from: "0.0.0"),
+		.package(url: "https://github.com/multitudes/AdventKit.git", from: "0.4.0"),
     ],
     targets: [
         .target(
