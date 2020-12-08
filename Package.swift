2270e433bc5fb31239bf0f3ea3dfcfb3b60fc7da
laurent <wrmultitudes@gmail.com>
2020-12-08 10:28:23 +0100

    day8

--- Package.swift
+++ Package.swift
@@ -68,6 +68,13 @@ let package = Package(
 			exclude: ["README.md"],
 			resources: [.process("Resources")]
 		),
+		.target(
+			name: "Day8",
+			dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser"),
+				.product(name: "AdventKit", package: "AdventKit")],
+			exclude: ["README.md"],
+			resources: [.process("Resources")]
+		),
 		.testTarget(
 			name: "Day2Tests",
 			dependencies: ["AdventOfCode2020"]),
---
+++ Sources/Day8/main.swift
@@ -0,0 +1,38 @@
+import ArgumentParser
+import Foundation
+import AdventKit
+
+// Define our parser.
+struct Day8: ParsableCommand {
+	//Declare optional argument. Drag the input file to terminal!
+	@Option(name: [.short, .customLong("inputFile")], help: "Specify the path to the input file.")
+	var inputFile : String = ""
+
+	func run() throws {
+		var input: String = ""
+
+		if !inputFile.isEmpty {
+			let url = URL(fileURLWithPath: inputFile)
+			guard let inputString = try? String(contentsOf: url) else {throw RuntimeError("Couldn't read from file!")}
+			input = inputString
+		} else {
+			print("Running Day8 Challenge with input from the website\n")
+			guard let url = Bundle.module.url(forResource: "input", withExtension: "txt") else {
+				fatalError("Input file not found")
+			}
+			guard let inputString = try? String(contentsOf: url) else {fatalError("input not valid")}
+			input = inputString
+		}
+		var solution: [String] = []
+
+		print("\nThe solution for the first challenge is: ", solution.count)
+
+		let solution2 = 0
+		print("\nThe solution for the second challenge is: ", solution2)
+	}
+}
+
+// Run the parser.
+Day8.main()
+
+
