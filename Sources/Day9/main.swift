f280f707f3ce80ef75e06e0d476a4fcd949e1d6e
laurent <wrmultitudes@gmail.com>
2020-12-09 15:41:53 +0100

    Update main.swift

--- Sources/Day9/main.swift
+++ Sources/Day9/main.swift
@@ -24,17 +24,28 @@ struct Day9: ParsableCommand {
 			guard let inputLines = try? String(contentsOf: url).lines.compactMap(Int.init) else {fatalError("input not valid")}
 			input = inputLines
 		}
+
+		//check the workingQ is valid!
+		func checksums(queue: Array<Int>.SubSequence, with next: Int) -> (isValid:Bool, solution: Int? ) {
+			let q = queue.sorted()
+			let lastIndex = q.count - 1
+			var first:Int = 0; var last: Int = lastIndex
+			if q[lastIndex] + q[lastIndex-1] < next {return (false, next)}
+			while first < last {
+				let sum = q[first] + q[last]
+				switch sum  {
+					case next : return (true, nil)
+					case (..<next): first += 1;	continue
+					default: last -= 1;	continue
+				}
+			}
+			return (false, next)
+		}
 		func checkForRange(with invalidNumber: Int) -> Int? {
 			for i in 0..<count {
 				var partialSum = 0
 				var subSequence: Array<Int>.SubSequence = []
 				runningIndex = i
-				if input[runningIndex] + input[runningIndex + 1] == invalidNumber {
-					return input[runningIndex] + input[runningIndex + 1]
-				}
-				if input[runningIndex] + input[runningIndex + 1] > invalidNumber {
-					continue
-				}
 				while runningIndex < count - 1 {
 					let currentNumber = input[runningIndex]
 					runningIndex += 1; partialSum += currentNumber
@@ -48,63 +59,18 @@ struct Day9: ParsableCommand {
 			}
 			return nil
 		}
-		//check the workingQ is valid!
-		func checksums(queue: Array<Int>.SubSequence, with next: Int) -> (isValid:Bool, solution: Int? ) {
-			let q = queue.sorted()
-			print("working queue sorted ", q)
-			let lastIndex = q.count - 1
-			var first:Int = 0; var last: Int = lastIndex
-			let lasttwosum = q[lastIndex] + q[lastIndex-1]
-			if lasttwosum < next {
-				print("Error // solution is ",next); return (false, next) }
-
-			while first < last {
-				let sum = q[first] + q[last]
-				switch sum  {
-					case next :
-						print("q is ok /// ")
-						return (true, nil)
-					case (..<next) :
-						first += 1
-						continue
-					default:
-						last -= 1
-						continue
-				}
-			}
-			print("Error // solution is ",next)
-			return (false, next)
-		}
-
-		let preamble = 25
-		var workingQueue = input.prefix(preamble)
-		var runningIndex = preamble
-		let count = input.count
-
-		var solution1 = 0
-		var solution2 = 0
-
+		let preamble = 25; var workingQueue = input.prefix(preamble)
+		var runningIndex = preamble; let count = input.count
 		while runningIndex < count {
 			let next = input[runningIndex]
-			print("workingQueue", workingQueue, " next ", next)
-
-			if checksums(queue: workingQueue, with: next).isValid { // (true, nil)
-				print("ok")
-			} else {
-				print("ok", next, ">>>>>>>>>>>>>>>>>>>>>>>>")
-				solution1 = next
-				if let solution = checkForRange(with: next)  { print("Solution2 ! ")
-					solution2 = solution
-				}
+			if !checksums(queue: workingQueue, with: next).isValid {
+				print("Solution part 1: ", next) //18272118
+				print("Solution part 2: ", checkForRange(with: next) ?? 0) // 2186361
 				break
 			}
 			workingQueue = workingQueue.dropFirst()
-			workingQueue.append(next)
-			runningIndex += 1
+			workingQueue.append(next); runningIndex += 1
 		}
-
-		print("Solution part 1: ", solution1) //18272118
-		print("Solution part 2: ", solution2) // 2186361
 	}
 }
 
