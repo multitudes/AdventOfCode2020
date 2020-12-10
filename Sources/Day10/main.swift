3682596538cd87fac108cac75b4548776825643b
laurent <wrmultitudes@gmail.com>
2020-12-10 14:53:42 +0100

    Update main.swift

--- Sources/Day10/main.swift
+++ Sources/Day10/main.swift
@@ -24,6 +24,7 @@ struct Day10: ParsableCommand {
 			guard let inputLines = try? String(contentsOf: url).lines.compactMap(Int.init) else {fatalError("input not valid")}
 			input = inputLines.sorted()
 		}
+
 		var jolts: [Int: Int] = [:]
 		var previousOutput = 0
 
@@ -39,7 +40,25 @@ struct Day10: ParsableCommand {
 
 		let solution = jolts1 * jolts3 //1904
 		print("Solution part 1: ", solution)
+
+
+		let currentAdapter = 0
+		print(input[currentAdapter+1...currentAdapter+3])
+		func checkAdaptersInRange(with currentAdapter: Int) -> Int {
+			if currentAdapter == input.last! { return 1 }
+			var validSequences = 0
+			for adapter in input where adapter - currentAdapter <= 3 {
+				if Range(currentAdapter+1...currentAdapter+3) ~= adapter {
+					validSequences += checkAdaptersInRange(with: adapter)
+				}
+			}
+			return validSequences
+		}
+
+		let solution2 = checkAdaptersInRange(with: currentAdapter)
+		print("Solution part 2: ", solution2)
 	}
+
 }
 
 // Run the parser.
