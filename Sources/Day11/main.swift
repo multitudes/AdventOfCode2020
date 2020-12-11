2992c6177563597e679e438d9f763db767e4b5a6
laurent <wrmultitudes@gmail.com>
2020-12-11 20:04:57 +0100

    Update main.swift

--- Sources/Day11/main.swift
+++ Sources/Day11/main.swift
@@ -30,8 +30,6 @@ struct Day10: ParsableCommand {
 				return seatState
 			}
 		}
-
-
 		// create seatmap with padding
 		var seatMap = input.compactMap { string -> [Character]? in
 			if !string.isEmpty {
@@ -40,14 +38,30 @@ struct Day10: ParsableCommand {
 			}
 			return nil
 		}
+
 		let inputColumns = seatMap[0].count
 		let padding = Array(repeating: Character(" "), count: inputColumns)
 		seatMap.insert(padding, at: 0)
 		seatMap.append(padding)
-		print(seatMap)
 
-		func checkAdjacentsAreOccupied(row i: Int, col k: Int) -> Int {
+		func checkAdjacentsAreOccupied(row i: Int, col k: Int, partTwo: Bool ) -> Int {
 			var adjacents = 0
+			let directions: [(x: Int, y: Int)] = [(x: -1,y: -1),(x: 0, y: -1),(x: 1,y: -1),(x: -1,y: 0),(x: 1,y: 0),(x: -1,y: 1),(x: 0, y: 1),(x: 1,y: 1)]
+			if partTwo {
+				for direction in directions {
+					var xOffset = direction.x; var yOffset = direction.y
+					var step = 0
+					while true {
+						step += 1
+						xOffset = step * direction.x ; yOffset = step * direction.y
+						if seatMap[i+yOffset][k+xOffset] == SeatState.padding.rawValue {break}
+						if seatMap[i+yOffset][k+xOffset] == SeatState.floor.rawValue {}
+						if seatMap[i+yOffset][k+xOffset] == SeatState.empty.rawValue {break}
+						if seatMap[i+yOffset][k+xOffset] == SeatState.occupied.rawValue {adjacents += 1; break}
+					}
+				}
+				return adjacents
+			} else {
 			if seatMap[i-1][k-1] == SeatState.occupied.rawValue { adjacents += 1 }
 			if seatMap[i-1][k] == SeatState.occupied.rawValue {adjacents += 1 }
 			if seatMap[i-1][k+1] == SeatState.occupied.rawValue {adjacents += 1 }
@@ -57,24 +71,23 @@ struct Day10: ParsableCommand {
 			if seatMap[i+1][k] == SeatState.occupied.rawValue {adjacents += 1 }
 			if seatMap[i+1][k+1] == SeatState.occupied.rawValue {adjacents += 1 }
 			return adjacents
+			}
 		}
-		let maxColumns = seatMap[0].count
-		let maxRows = seatMap.count
-		//start
 
-		func oneSeatingShuffle(_ seatMap: [[Character]], with currentSeat: SeatState ) ->  (nextMap: [[Character]], isSameState: Bool, occupiedSeats: Int) {
-			var nextSeatMap = seatMap
-			var occupiedSeats = 0
+		func oneSeatingShuffle(_ seatMap: [[Character]], with currentSeat: SeatState, is partTwo: Bool = false ) ->  (nextMap: [[Character]], isSameState: Bool, occupiedSeats: Int) {
+			let maxColumns = seatMap[0].count; let maxRows = seatMap.count
+			var nextSeatMap = seatMap; var occupiedSeats = 0
+			var maxVisibleOccupiedSeats: Int = 4; if partTwo { maxVisibleOccupiedSeats = 5}
+			//print("= ======= part 2 ",maxVisibleOccupiedSeats )
 			for i in 1..<maxRows - 1 {
-				print(seatMap[i])
 				for k in 1..<maxColumns - 1 {
 					if seatMap[i][k] == SeatState.occupied.rawValue {occupiedSeats += 1 }
 					if ". ".contains(seatMap[i][k]) {continue}
-					let adjacents = checkAdjacentsAreOccupied(row: i, col: k)
+					let adjacents = checkAdjacentsAreOccupied(row: i, col: k, partTwo: partTwo)
 					if currentSeat == SeatState.empty {
 						if adjacents == 0 {	nextSeatMap[i][k] = SeatState.occupied.rawValue }
 					} else if currentSeat == SeatState.occupied {
-						if adjacents >= 4  { nextSeatMap[i][k] = SeatState.empty.rawValue }
+						if adjacents >= maxVisibleOccupiedSeats  { nextSeatMap[i][k] = SeatState.empty.rawValue }
 					}
 				}
 			}
@@ -83,17 +96,25 @@ struct Day10: ParsableCommand {
 			print("same than last? ",seatMap == nextSeatMap, "occupiedSeats", occupiedSeats)
 			return (nextMap: nextSeatMap, isSameState: seatMap == nextSeatMap, occupiedSeats: occupiedSeats)
 		}
+
+
 		var isSame = false
 		var seatState = SeatState(rawValue: "L")!
 		var occupiedSeats = 0
+		//while isSame == false {
+		//	(seatMap, isSame, occupiedSeats) = oneSeatingShuffle(seatMap, with: seatState)
+		//	seatState = SeatState.toggle(seatState)
+		//}
+		//print("solution: ", occupiedSeats)
+
+		isSame = false
+		var partTwo: Bool = true
 		while isSame == false {
-
-			(seatMap, isSame, occupiedSeats) = oneSeatingShuffle(seatMap, with: seatState)
+			(seatMap, isSame, occupiedSeats) = oneSeatingShuffle(seatMap, with: seatState, is: partTwo)
 			seatState = SeatState.toggle(seatState)
-
 		}
-
 		print("solution: ", occupiedSeats)
+
 	}
 }
 
