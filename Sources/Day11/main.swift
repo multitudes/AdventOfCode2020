cb60f5ff0309ac4fa2e60aa2bedca76e3225ae8a
laurent <wrmultitudes@gmail.com>
2020-12-12 13:02:37 +0100

    Update main.swift

--- Sources/Day11/main.swift
+++ Sources/Day11/main.swift
@@ -13,7 +13,7 @@ struct Day11: ParsableCommand {
 		var input: [String] = []
 		if !inputFile.isEmpty {
 			let url = URL(fileURLWithPath: inputFile)
-			guard let inputFile = try? String(contentsOf: url).lines.compactMap(Int.init) else {fatalError()}
+			guard let inputFile = try? String(contentsOf: url).components(separatedBy: .whitespacesAndNewlines) else {fatalError()}
 			input = inputFile
 		} else {
 			print("Running Day11 Challenge with input from the website\n")
@@ -26,18 +26,21 @@ struct Day11: ParsableCommand {
 
 		enum SeatState: Character {
 			case occupied = "#", empty = "L", floor = ".", padding = " "
+
+			static var isSame = false;	static var seatState = SeatState.empty;
+			static var occupiedSeats = 0;
 			static func toggle(_ seatState: SeatState) -> SeatState {
 				if seatState == .occupied {return .empty }
 				if seatState == .empty {return .occupied }
 				return seatState
 			}
+			static func resetState() {
+				isSame = false;	seatState = SeatState.empty; occupiedSeats = 0;
+			}
 		}
-
-		var isSame = false;	var seatState = SeatState.empty; var occupiedSeats = 0
-		var seatMap:[[Character]] = []
-
-		// create seatmap with padding
-		func createSeatMapWithPadding() -> [[Character]]{
+		
+		// create new seatmap with padding
+		func createSeatMapWithPadding() -> [[Character]] {
 			var seatMap = input.compactMap { string -> [Character]? in
 				if !string.isEmpty {
 					let newString = " " + string + " "
@@ -56,38 +59,28 @@ struct Day11: ParsableCommand {
 		func checkAdjacentsAreOccupied(row i: Int, col k: Int, partTwo: Bool ) -> Int {
 			var adjacents = 0
 			let directions: [(x: Int, y: Int)] = [(x: -1,y: -1),(x: 0, y: -1),(x: 1,y: -1),(x: -1,y: 0),(x: 1,y: 0),(x: -1,y: 1),(x: 0, y: 1),(x: 1,y: 1)]
-			if partTwo {
-				for direction in directions {
-					var xOffset = direction.x; var yOffset = direction.y
-					var step = 0
-					while true {
-						step += 1
-						xOffset = step * direction.x ; yOffset = step * direction.y
-						if seatMap[i+yOffset][k+xOffset] == SeatState.padding.rawValue {break}
-						if seatMap[i+yOffset][k+xOffset] == SeatState.floor.rawValue {}
-						if seatMap[i+yOffset][k+xOffset] == SeatState.empty.rawValue {break}
-						if seatMap[i+yOffset][k+xOffset] == SeatState.occupied.rawValue {adjacents += 1; break}
-					}
+
+			for direction in directions {
+				var xOffset = direction.x; var yOffset = direction.y
+				var step = 0
+				while true {
+					step += 1
+					xOffset = step * direction.x ; yOffset = step * direction.y
+					if seatMap[i+yOffset][k+xOffset] == SeatState.padding.rawValue {break}
+					if seatMap[i+yOffset][k+xOffset] == SeatState.floor.rawValue {}
+					if seatMap[i+yOffset][k+xOffset] == SeatState.empty.rawValue {break}
+					if seatMap[i+yOffset][k+xOffset] == SeatState.occupied.rawValue {adjacents += 1; break}
+					if !partTwo { break}
 				}
-				return adjacents
-			} else {
-			if seatMap[i-1][k-1] == SeatState.occupied.rawValue { adjacents += 1 }
-			if seatMap[i-1][k] == SeatState.occupied.rawValue {adjacents += 1 }
-			if seatMap[i-1][k+1] == SeatState.occupied.rawValue {adjacents += 1 }
-			if seatMap[i][k-1] == SeatState.occupied.rawValue {adjacents += 1 }
-			if seatMap[i][k+1] == SeatState.occupied.rawValue {adjacents += 1 }
-			if seatMap[i+1][k-1] == SeatState.occupied.rawValue {adjacents += 1 }
-			if seatMap[i+1][k] == SeatState.occupied.rawValue {adjacents += 1 }
-			if seatMap[i+1][k+1] == SeatState.occupied.rawValue {adjacents += 1 }
-			return adjacents
 			}
+			return adjacents
 		}
 
 		func oneSeatingShuffle(_ seatMap: [[Character]], with currentSeat: SeatState, partTwo: Bool = false ) ->  (nextMap: [[Character]], isSameState: Bool, occupiedSeats: Int) {
 			let maxColumns = seatMap[0].count; let maxRows = seatMap.count
 			var nextSeatMap = seatMap; var occupiedSeats = 0
 			var maxVisibleOccupiedSeats: Int = 4; if partTwo { maxVisibleOccupiedSeats = 5}
-			//print("= ======= part 2 ",maxVisibleOccupiedSeats )
+
 			for i in 1..<maxRows - 1 {
 				for k in 1..<maxColumns - 1 {
 					if seatMap[i][k] == SeatState.occupied.rawValue {occupiedSeats += 1 }
@@ -102,28 +95,29 @@ struct Day11: ParsableCommand {
 			}
 			for map in nextSeatMap {
 				print(map.map { String($0)}.joined())}
-//			print("same than last? ",seatMap == nextSeatMap, "occupiedSeats", occupiedSeats)
 			return (nextMap: nextSeatMap, isSameState: seatMap == nextSeatMap, occupiedSeats: occupiedSeats)
 		}
 
-		// part 1 --
-		func resetState() {
-			isSame = false;	seatState = SeatState.empty; occupiedSeats = 0; seatMap = createSeatMapWithPadding()
-		}
-		resetState()
-		while isSame == false {
-			(seatMap, isSame, occupiedSeats) = oneSeatingShuffle(seatMap, with: seatState)
-			seatState = SeatState.toggle(seatState)
-		}
-		let solution1 = occupiedSeats
+		var seatMap:[[Character]] = []
 
-		resetState()
-		while isSame == false {
-			(seatMap, isSame, occupiedSeats) = oneSeatingShuffle(seatMap, with: seatState, partTwo: true)
-			seatState = SeatState.toggle(seatState)
+		// part 1 --
+		SeatState.resetState()
+		seatMap = createSeatMapWithPadding()
+		while SeatState.isSame == false {
+			(seatMap, SeatState.isSame, SeatState.occupiedSeats) = oneSeatingShuffle(seatMap, with: .seatState)
+			SeatState.seatState = SeatState.toggle(.seatState)
+		}
+		let solution1 = SeatState.occupiedSeats
+
+		// part 2 --
+		SeatState.resetState()
+		seatMap = createSeatMapWithPadding()
+		while SeatState.isSame == false {
+			(seatMap, SeatState.isSame, SeatState.occupiedSeats) = oneSeatingShuffle(seatMap, with: .seatState, partTwo: true)
+			SeatState.seatState = SeatState.toggle(.seatState)
 		}
 		print("Solution part 1: ", solution1) // 2354
-		print("Solution part 2: ", occupiedSeats) //2072
+		print("Solution part 2: ", SeatState.occupiedSeats) //2072
 
 	}
 }
