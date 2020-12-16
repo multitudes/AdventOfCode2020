c117332f7a2034b5e68335b389fbac13a79514ca
laurent <wrmultitudes@gmail.com>
2020-12-16 11:41:42 +0100

    day16

--- Sources/Day16/main.swift
+++ Sources/Day16/main.swift
@@ -8,12 +8,16 @@ struct Day16: ParsableCommand {
 	//Declare optional argument. Drag the input file to terminal!
 	@Option(name: [.short, .customLong("inputFile")], help: "Specify the path to the input file.")
 	var inputFile : String = ""
+
 	
+
 	func run() throws {
 
 
-		print("Solution part 1: ") //1065
-		print("Solution part 2: ") // 1065
+
+		print("Solution part 1: "
+			  ,2**3) //
+		print("Solution part 2: ") //
 
 	}
 }
