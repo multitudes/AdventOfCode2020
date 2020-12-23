bf016ce3bbc574260f74e2db9b8c561f6d86f431
laurent <wrmultitudes@gmail.com>
2020-12-23 18:16:22 +0100

    day21

--- Sources/Day21/main.swift
+++ Sources/Day21/main.swift
@@ -4,239 +4,113 @@ import AdventKit
 
 
 // Define our parser.
-struct Day17: ParsableCommand {
+struct Day21: ParsableCommand {
 	//Declare optional argument. Drag the input file to terminal!
 	@Option(name: [.short, .customLong("inputFile")], help: "Specify the path to the input file.")
 	var inputFile : String = ""
 	
 	func run() throws {
-		var input: String = ""
+		var input: [(ingredients: Set<String>, allergens: Set<String>)] = []
 		if !inputFile.isEmpty {
 			let url = URL(fileURLWithPath: inputFile)
-			guard let inputFile = try? String(contentsOf: url) else {fatalError()}
-			input = inputFile
+			guard let inputFile = try? String(contentsOf: url).replacingOccurrences(of: "[,)]", with: "", options: .regularExpression).linesNotEmpty else {fatalError()}
+			input = inputFile.map {$0.split(separator: " ")
+				.map {String($0)}.split {$0 == "(contains"}}
+				.map {return (ingredients: Set($0[0]),allergens: Set($0[1]))}
+			input.sort {$0.allergens.count < $1.allergens.count}
 		} else {
-			print("Running Day17 Challenge with input from the website\n")
+			print("Running Day21 Challenge with input from the website\n")
 			guard let url = Bundle.module.url(forResource: "input", withExtension: "txt") else { fatalError()}
 			//guard let url = Bundle.module.url(forResource: "Day17-example", withExtension: "txt") else { fatalError()}
-
-			guard let inputFile = try? String(contentsOf: url) else {fatalError()}
-			input = inputFile
+			guard let inputFile = try? String(contentsOf: url).replacingOccurrences(of: "[,)]", with: "", options: .regularExpression).linesNotEmpty else {fatalError()}
+			input = inputFile.map { $0.split(separator: " ")
+				.map {String($0) }
+				.split { $0 == "(contains"}}
+				.map { return (ingredients: Set($0[0]),allergens: Set($0[1]))}
+				input.sort {$0.allergens.count < $1.allergens.count}
 		}
 		print(input)
-		let partOne = false
-		if partOne {
-			struct Cube: Equatable, Hashable {
-				let x,y,z: Int
+
+
+
+		//print(input.description)
+		var myCanonicalDangerousIngredients: [(ingredient: String, allergen: String)] = []
+
+		func removeEverywhere(ingredientFound: String, allergen: String) {
+			// Part 2! add allergen to the list
+			myCanonicalDangerousIngredients.append((ingredient: ingredientFound, allergen: allergen))
+			for j in 0..<input.count {
+				// when match found - 2 cases - both allergen and ingredient to be removed or
+				if input[j].ingredients.contains(ingredientFound)
+					&& input[j].allergens.contains(allergen) {
+					print("removing input[\(j)]", ingredientFound, allergen)
+					let _ = input[j].ingredients.remove(ingredientFound)!
+					let _ = input[j].allergens.remove(allergen)!
+					print("removed" )
+					// or allergen is not listed so I remove only the ingredient
+				} else if input[j].ingredients.contains(ingredientFound) {
+					print("removing input[\(j)]", ingredientFound)
+					let _ = input[j].ingredients.remove(ingredientFound)!
+				}
 			}
-
-			struct PocketDimension {
-				var actives : Set<Cube> = []
-				var inactives: Set<Cube> = []
-				var nextActives : Set<Cube> = []
-				var nextInactives: Set<Cube> = []
-				static var cycles = 0
-				init(input: String) {
-					let bootSector = input.lines
-					for (yIndex, y) in bootSector.enumerated() {
-						print(y)
-						for (xIndex,x)  in y.enumerated() {
-							if x == "#" { actives.insert(Cube(x: xIndex, y: yIndex, z: 0))} else {
-								inactives.insert(Cube(x: xIndex, y: yIndex, z: 0)); continue}
-						}
-					}
-				}
-
-				func printBootSector() {
-					for z in -Self.cycles...Self.cycles {
-						print("\nLayer \(z)")
-						for y in -10...10 {
-							for x in -10...10 {
-								let cube = Cube(x: x, y: y, z: z)
-								if actives.contains(cube) {print("#", terminator: "")} else {print(".",terminator: "")}
-							}
-							print("\n", terminator: "")
-						}
-					}
-				}
-
-				mutating func runCycle() {
-					for cube in actives {
-						var count = 0
-						for z in -1...1 {
-							for y in -1...1 {
-								for x in -1...1 {
-									//print("x y z ", x, y, z, "....................")
-									// this is my current position so skip
-									if x == 0 && y == 0 && z == 0 { continue }
-									//create an imaginary cube as neighbour
-									let cube = Cube(x: (cube.x + x), y: (cube.y + y), z: (cube.z + z))
-									// if this position is not already active append inactive
-									if actives.contains(cube) {
-										count += 1
-									} else {
-										inactives.insert(cube)
-									}
-								}
-							}
-						}
-						if 2...3 ~= count { nextActives.insert(cube) } else {
-							nextInactives.insert(cube)
-						}
-					}
-
-					for cube in inactives {
-						var count = 0
-						for z in -1...1 {
-							for y in -1...1 {
-								for x in -1...1 {
-									//print("x y z ", x, y, z, "....................")
-									// this is my current position so skip
-									if x == 0 && y == 0 && z == 0 { continue }
-									//create an imaginary cube as neighbour
-									let cube = Cube(x: (cube.x + x), y: (cube.y + y), z: (cube.z + z))
-									// if this position is not already active append inactive
-									if actives.contains(cube) {
-										count += 1
-									} else {
-										nextInactives.insert(cube)
-									}
-								}
-							}
-						}
-						if count == 3 {nextActives.insert(cube)} else {nextInactives.insert(cube)}
-					}
-					actives = nextActives
-					nextActives = []
-					inactives = nextInactives
-					nextInactives = []
-					Self.cycles += 1
-					printBootSector()
-				}
-
-			}
-
-			var boot = PocketDimension(input: input)
-			print(boot.printBootSector())
-			boot.runCycle()
-			boot.runCycle()
-			boot.runCycle()
-			boot.runCycle()
-			boot.runCycle()
-			boot.runCycle()
-			print("Solution Part One : ",boot.actives.count)
-
-		} else {
-			struct Cube: Equatable, Hashable {let x,y,z,w: Int}
-
-			struct PocketDimension {
-				var actives : Set<Cube> = []
-				var inactives: Set<Cube> = []
-				var nextActives : Set<Cube> = []
-				var nextInactives: Set<Cube> = []
-				static var cycles = 0
-
-				init(input: String) {
-					let bootSector = input.lines
-					for (yIndex, y) in bootSector.enumerated() {
-						for (xIndex,x)  in y.enumerated() {
-							if x == "#" { actives.insert(Cube(x: xIndex, y: yIndex, z: 0, w: 0))} else {
-								inactives.insert(Cube(x: xIndex, y: yIndex, z: 0, w: 0)); continue}
-						}
-					}
-				}
-
-				func printBootSector() {
-					for w in -Self.cycles...Self.cycles {
-						for z in -Self.cycles...Self.cycles {
-							print("\nLayer \(z), \(w)")
-							for y in -10...10 {
-								for x in -10...10 {
-									let cube = Cube(x: x, y: y, z: z, w: w)
-									if actives.contains(cube) {print("#", terminator: "")} else {print(".",terminator: "")}
-								}
-								print("\n", terminator: "")
-							}
-						}
-					}
-				}
-
-				mutating func runCycle() {
-					for cube in actives {
-						var count = 0
-						for w in -1...1 {
-							for z in -1...1 {
-								for y in -1...1 {
-									for x in -1...1 {
-										// this is my current position so skip
-										if x == 0 && y == 0 && z == 0 && w == 0 { continue }
-										//create an imaginary cube as neighbour
-										let neighbour = Cube(x: (cube.x + x), y: (cube.y + y), z: (cube.z + z), w: (cube.w + w))
-										// if this position is active count, if not active insert in inactives
-										if actives.contains(neighbour) {
-											count += 1
-											continue
-										} else {
-											inactives.insert(neighbour)
-										}
-									}
-								}
-							}
-						}
-						if 2...3 ~= count { nextActives.insert(cube) } else {
-							nextInactives.insert(cube)
-						}
-					}
-
-					for cube in inactives {
-						var count = 0
-						for w in -1...1 {
-							for z in -1...1 {
-								for y in -1...1 {
-									for x in -1...1 {
-											// this is my current position so skip
-										if x == 0 && y == 0 && z == 0 && w == 0{ continue }
-										//create an imaginary cube as neighbour
-										let neighbour = Cube(x: (cube.x + x), y: (cube.y + y), z: (cube.z + z), w: (cube.w + w))
-										// if this position is not active insert in next inactive
-										if actives.contains(neighbour) {
-											count += 1
-										} else {
-											nextInactives.insert(neighbour)
-										}
-									}
-								}
-							}
-						}
-						if count == 3 {nextActives.insert(cube)} else {nextInactives.insert(cube)}
-					}
-					actives = nextActives
-					nextActives = []
-					inactives = nextInactives
-					nextInactives = []
-					Self.cycles += 1
-					print("---------------- cycle \(Self.cycles) -------------")
-					//printBootSector()
-				}
-
-			}
-
-			var boot = PocketDimension(input: input)
-			print(boot.printBootSector())
-			boot.runCycle()
-			boot.runCycle()
-			boot.runCycle()
-			boot.runCycle()
-			boot.runCycle()
-			boot.runCycle()
-			print("Solution Part Two : ",boot.actives.count)
 		}
-	}
 
+		// my elements are sorted for max probability to get a single allergen in element
+		var k = -1
+		while !input.allSatisfy({ $0.allergens.isEmpty })   {
+			// looping until all ingredients corresponding to our allergens are found
+			k = k < input.count - 1 ? k + 1 : 0
+			let currentElement = input[k]
+			// I am looking for that element which has one allergen only
+			if currentElement.allergens.isEmpty { continue }
+			if currentElement.allergens.count > 1 { continue }
+			// this one is already a match!
+			if currentElement.ingredients.count == 1 && currentElement.allergens.count == 1 {
+				print("1-1 match = remove everywhere ")
+				removeEverywhere(ingredientFound: currentElement.ingredients.first!, allergen: currentElement.allergens.first!)
+				k = -1 // restart because some elements before might have gotten freed up
+				continue
+			}
+			// element with one allergen and many ingredients. need to check intersections! loop from the next element to end and look for an intersection. Then take the results of that intersection and intersect with the ingredients of the next element matching the allergen and hope to get the 1-1 match
+			if currentElement.allergens.count == 1 {
+				var mainIntersecting: Set<String> = currentElement.ingredients // init
+				for i in 0..<input.count where i != k {
+					//look for an intersection and if found remove the ingredient and element
+					let currentAllergen = currentElement.allergens.first!
+					print("looking for allergen ", currentAllergen)
+					let elementToCheck = input[i]
+					if elementToCheck.allergens.contains(currentAllergen)  {
+						print("this contains : \(currentAllergen) ")
+						mainIntersecting = elementToCheck.ingredients.intersection(mainIntersecting)
+						print("ingredientsIntersecting ", mainIntersecting)
+					}
+					// this is a case of 1-1 match ingredients and allergen!
+					if mainIntersecting.count == 1 {
+						print("mainIntersecting.count == 1  ---------------------------------")
+						removeEverywhere(ingredientFound: mainIntersecting.first!, allergen: currentElement.allergens.first!)
+						break
+					}
+				}
+			}
+		}
+		// count the ingredients left!
+		let solutionPartOne = input.reduce(0) { $0 + $1.ingredients.count}
+
+		print("Solution part one : ", solutionPartOne)
+
+		//get the list of my Canonical Dangerous Ingredients)
+		let myCanonicalDangerousIngredientsList = myCanonicalDangerousIngredients.sorted {$0.allergen < $1.allergen}
+			.reduce("") {$0 + "," + $1.ingredient}.dropFirst() // drop first letter because that would be a comma! ;)
+
+		print("Solution part two : ", myCanonicalDangerousIngredientsList)
+	}
 }
 
-
 // Run the parser.
-Day17.main()
+Day21.main()
 
-
+extension String {
+	var linesNotEmpty: [String] {
+		components(separatedBy: .newlines).filter {$0 != "" }
+	}
+}
