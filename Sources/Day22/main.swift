89b9f4ed763fe48881eab6c5c8bb727c63e73c16
laurent <wrmultitudes@gmail.com>
2020-12-24 15:11:32 +0100

    day22

--- Sources/Day22/main.swift
+++ Sources/Day22/main.swift
@@ -4,239 +4,126 @@ import AdventKit
 
 
 // Define our parser.
-struct Day17: ParsableCommand {
+struct Day22: ParsableCommand {
 	//Declare optional argument. Drag the input file to terminal!
 	@Option(name: [.short, .customLong("inputFile")], help: "Specify the path to the input file.")
 	var inputFile : String = ""
 	
 	func run() throws {
-		var input: String = ""
+		var input: [[String]] = []
 		if !inputFile.isEmpty {
 			let url = URL(fileURLWithPath: inputFile)
-			guard let inputFile = try? String(contentsOf: url) else {fatalError()}
-			input = inputFile
+			guard let inputFile = try? String(contentsOf: url).lines else {fatalError()}
+			input = inputFile.split {$0 == "" }.map {Array($0) }
 		} else {
 			print("Running Day17 Challenge with input from the website\n")
 			guard let url = Bundle.module.url(forResource: "input", withExtension: "txt") else { fatalError()}
-			//guard let url = Bundle.module.url(forResource: "Day17-example", withExtension: "txt") else { fatalError()}
+			//guard let url = Bundle.module.url(forResource: "Day22-example", withExtension: "txt") else { fatalError()}
 
-			guard let inputFile = try? String(contentsOf: url) else {fatalError()}
-			input = inputFile
+			guard let inputFile = try? String(contentsOf: url).lines else {fatalError()}
+			input = inputFile.split {$0 == "" }.map {Array($0) }
 		}
+
 		print(input)
-		let partOne = false
-		if partOne {
-			struct Cube: Equatable, Hashable {
-				let x,y,z: Int
+
+		// init
+		let one = input[0].compactMap {Int($0)}
+		let two = input[1].compactMap {Int($0)}
+
+		//while !one.isEmpty && !two.isEmpty {
+		//	func playRound() {
+		//		let topOne = one.remove(at: 0); let topTwo = two.remove(at: 0)
+		//		if topOne > topTwo {
+		//			one.append(contentsOf: [topOne, topTwo])
+		//		} else {
+		//			two.append(contentsOf: [topTwo, topOne])
+		//		}
+		//	}
+		//	playRound()
+		//}
+		//
+		//one
+		//two
+		//
+		//let range = Range(1...two.count).reversed()
+		//let sol = zip(range, two).reduce(0) { $0 + $1.0 * $1.1 }
+		//
+		//
+		//print("Solution part one : ", sol)
+
+
+		// start new game
+		var game = 0
+		var winningDeck: [Int] = []
+		func playGame(deck1 one: [Int], deck2 two: [Int]) -> Int {
+			var oneCopy = one; var twoCopy = two; game += 1
+			var currentGameHistory: Set<String> = [] //init
+			var gamesEnd = false
+
+			while !oneCopy.isEmpty && !twoCopy.isEmpty {
+				print("/n-----------Game \(game)----------")
+				print("Player 1's deck: ", oneCopy)
+				print("Player 2's deck: ", twoCopy)
+
+				// check history!
+				let historyHash = (oneCopy.map {String($0)}.joined(separator: ".") + "-"
+					+ twoCopy.map {String($0)}.joined(separator: "."))
+				if !currentGameHistory.insert(historyHash).inserted {
+					print("Cards in history! this game won by player one!")
+					gamesEnd = true; break
+				} else {
+					//print("cards inserted in history")
+				}
+
+				let topOne = oneCopy.remove(at: 0); let topTwo = twoCopy.remove(at: 0)
+
+				// check if recursion
+				if topOne <= oneCopy.count && topTwo <= twoCopy.count {
+					print("Recursive Combat")
+					game += 1
+					let gameResult = playGame(deck1: Array(oneCopy.prefix(topOne)), deck2: Array(twoCopy.prefix(topTwo)))
+
+					if gameResult == 1 {
+						oneCopy.append(contentsOf: [topOne, topTwo])
+					} else {
+						twoCopy.append(contentsOf: [topTwo, topOne])
+					}
+					continue
+				}
+
+				// if no recursion play the game
+				if topOne > topTwo {
+					oneCopy.append(contentsOf: [topOne, topTwo])
+				} else {
+					twoCopy.append(contentsOf: [topTwo, topOne])
+				}
 			}
-
-			struct PocketDimension {
-				var actives : Set<Cube> = []
-				var inactives: Set<Cube> = []
-				var nextActives : Set<Cube> = []
-				var nextInactives: Set<Cube> = []
-				static var cycles = 0
-				init(input: String) {
-					let bootSector = input.lines
-					for (yIndex, y) in bootSector.enumerated() {
-						print(y)
-						for (xIndex,x)  in y.enumerated() {
-							if x == "#" { actives.insert(Cube(x: xIndex, y: yIndex, z: 0))} else {
-								inactives.insert(Cube(x: xIndex, y: yIndex, z: 0)); continue}
-						}
-					}
-				}
-
-				func printBootSector() {
-					for z in -Self.cycles...Self.cycles {
-						print("\nLayer \(z)")
-						for y in -10...10 {
-							for x in -10...10 {
-								let cube = Cube(x: x, y: y, z: z)
-								if actives.contains(cube) {print("#", terminator: "")} else {print(".",terminator: "")}
-							}
-							print("\n", terminator: "")
-						}
-					}
-				}
-
-				mutating func runCycle() {
-					for cube in actives {
-						var count = 0
-						for z in -1...1 {
-							for y in -1...1 {
-								for x in -1...1 {
-									//print("x y z ", x, y, z, "....................")
-									// this is my current position so skip
-									if x == 0 && y == 0 && z == 0 { continue }
-									//create an imaginary cube as neighbour
-									let cube = Cube(x: (cube.x + x), y: (cube.y + y), z: (cube.z + z))
-									// if this position is not already active append inactive
-									if actives.contains(cube) {
-										count += 1
-									} else {
-										inactives.insert(cube)
-									}
-								}
-							}
-						}
-						if 2...3 ~= count { nextActives.insert(cube) } else {
-							nextInactives.insert(cube)
-						}
-					}
-
-					for cube in inactives {
-						var count = 0
-						for z in -1...1 {
-							for y in -1...1 {
-								for x in -1...1 {
-									//print("x y z ", x, y, z, "....................")
-									// this is my current position so skip
-									if x == 0 && y == 0 && z == 0 { continue }
-									//create an imaginary cube as neighbour
-									let cube = Cube(x: (cube.x + x), y: (cube.y + y), z: (cube.z + z))
-									// if this position is not already active append inactive
-									if actives.contains(cube) {
-										count += 1
-									} else {
-										nextInactives.insert(cube)
-									}
-								}
-							}
-						}
-						if count == 3 {nextActives.insert(cube)} else {nextInactives.insert(cube)}
-					}
-					actives = nextActives
-					nextActives = []
-					inactives = nextInactives
-					nextInactives = []
-					Self.cycles += 1
-					printBootSector()
-				}
-
+			print("...anyway, back to previous game ")
+			game -= 1
+			if twoCopy.isEmpty || gamesEnd {
+				winningDeck = oneCopy
+				return 1
+			} else {
+				winningDeck = twoCopy
+				return 2
 			}
-
-			var boot = PocketDimension(input: input)
-			print(boot.printBootSector())
-			boot.runCycle()
-			boot.runCycle()
-			boot.runCycle()
-			boot.runCycle()
-			boot.runCycle()
-			boot.runCycle()
-			print("Solution Part One : ",boot.actives.count)
-
-		} else {
-			struct Cube: Equatable, Hashable {let x,y,z,w: Int}
-
-			struct PocketDimension {
-				var actives : Set<Cube> = []
-				var inactives: Set<Cube> = []
-				var nextActives : Set<Cube> = []
-				var nextInactives: Set<Cube> = []
-				static var cycles = 0
-
-				init(input: String) {
-					let bootSector = input.lines
-					for (yIndex, y) in bootSector.enumerated() {
-						for (xIndex,x)  in y.enumerated() {
-							if x == "#" { actives.insert(Cube(x: xIndex, y: yIndex, z: 0, w: 0))} else {
-								inactives.insert(Cube(x: xIndex, y: yIndex, z: 0, w: 0)); continue}
-						}
-					}
-				}
-
-				func printBootSector() {
-					for w in -Self.cycles...Self.cycles {
-						for z in -Self.cycles...Self.cycles {
-							print("\nLayer \(z), \(w)")
-							for y in -10...10 {
-								for x in -10...10 {
-									let cube = Cube(x: x, y: y, z: z, w: w)
-									if actives.contains(cube) {print("#", terminator: "")} else {print(".",terminator: "")}
-								}
-								print("\n", terminator: "")
-							}
-						}
-					}
-				}
-
-				mutating func runCycle() {
-					for cube in actives {
-						var count = 0
-						for w in -1...1 {
-							for z in -1...1 {
-								for y in -1...1 {
-									for x in -1...1 {
-										// this is my current position so skip
-										if x == 0 && y == 0 && z == 0 && w == 0 { continue }
-										//create an imaginary cube as neighbour
-										let neighbour = Cube(x: (cube.x + x), y: (cube.y + y), z: (cube.z + z), w: (cube.w + w))
-										// if this position is active count, if not active insert in inactives
-										if actives.contains(neighbour) {
-											count += 1
-											continue
-										} else {
-											inactives.insert(neighbour)
-										}
-									}
-								}
-							}
-						}
-						if 2...3 ~= count { nextActives.insert(cube) } else {
-							nextInactives.insert(cube)
-						}
-					}
-
-					for cube in inactives {
-						var count = 0
-						for w in -1...1 {
-							for z in -1...1 {
-								for y in -1...1 {
-									for x in -1...1 {
-											// this is my current position so skip
-										if x == 0 && y == 0 && z == 0 && w == 0{ continue }
-										//create an imaginary cube as neighbour
-										let neighbour = Cube(x: (cube.x + x), y: (cube.y + y), z: (cube.z + z), w: (cube.w + w))
-										// if this position is not active insert in next inactive
-										if actives.contains(neighbour) {
-											count += 1
-										} else {
-											nextInactives.insert(neighbour)
-										}
-									}
-								}
-							}
-						}
-						if count == 3 {nextActives.insert(cube)} else {nextInactives.insert(cube)}
-					}
-					actives = nextActives
-					nextActives = []
-					inactives = nextInactives
-					nextInactives = []
-					Self.cycles += 1
-					print("---------------- cycle \(Self.cycles) -------------")
-					//printBootSector()
-				}
-
-			}
-
-			var boot = PocketDimension(input: input)
-			print(boot.printBootSector())
-			boot.runCycle()
-			boot.runCycle()
-			boot.runCycle()
-			boot.runCycle()
-			boot.runCycle()
-			boot.runCycle()
-			print("Solution Part Two : ",boot.actives.count)
 		}
+
+		let result = playGame(deck1: one, deck2: two)
+		result
+		print(winningDeck)
+		let range = Range(1...winningDeck.count).reversed()
+		let score = zip(range, winningDeck).reduce(0) { $0 + $1.0 * $1.1 }
+
+		print("score: ",score)
+		// 8017 not
+
 	}
 
 }
 
 
 // Run the parser.
-Day17.main()
+Day22.main()
 
 
--- Sources/Day24/README.md
+++ Sources/Day24/README.md
@@ -1,61 +1,46 @@
-# [Day 19: Monster Messages](https://adventofcode.com/2020/day/19)
+# [Day 24: Lobby Layout](https://adventofcode.com/2020/day/24)
 
 ## Part One
 
-You land in an airport surrounded by dense forest. As you walk to your high-speed train, the Elves at the Mythical Information Bureau contact you again. They think their satellite has collected an image of a sea monster! Unfortunately, the connection to the satellite is having problems, and many of the messages sent back from the satellite have been corrupted.
+Your raft makes it to the tropical island; it turns out that the small crab was an excellent navigator. You make your way to the resort.
 
-They sent you a list of the rules valid messages should obey and a list of received messages they've collected so far (your puzzle input).
+As you enter the lobby, you discover a small problem: the floor is being renovated. You can't even reach the check-in desk until they've finished installing the new tile floor.
 
-The rules for valid messages (the top part of your puzzle input) are numbered and build upon each other. For example:
-```
-0: 1 2
-1: "a"
-2: 1 3 | 3 1
-3: "b"
-```
-Some rules, like 3: "b", simply match a single character (in this case, b).
+The tiles are all hexagonal; they need to be arranged in a hex grid with a very specific color pattern. Not in the mood to wait, you offer to help figure out the pattern.
 
-The remaining rules list the sub-rules that must be followed; for example, the rule 0: 1 2 means that to match rule 0, the text being checked must match rule 1, and the text after the part that matched rule 1 must then match rule 2.
+The tiles are all white on one side and black on the other. They start with the white side facing up. The lobby is large enough to fit whatever pattern might need to appear there.
 
-Some of the rules have multiple lists of sub-rules separated by a pipe (|). This means that at least one list of sub-rules must match. (The ones that match might be different each time the rule is encountered.) For example, the rule 2: 1 3 | 3 1 means that to match rule 2, the text being checked must match rule 1 followed by rule 3 or it must match rule 3 followed by rule 1.
+A member of the renovation crew gives you a list of the tiles that need to be flipped over (your puzzle input). Each line in the list identifies a single tile that needs to be flipped by giving a series of steps starting from a reference tile in the very center of the room. (Every line starts from the same reference tile.)
 
-Fortunately, there are no loops in the rules, so the list of possible matches will be finite. Since rule 1 matches a and rule 3 matches b, rule 2 matches either ab or ba. Therefore, rule 0 matches aab or aba.
+Because the tiles are hexagonal, every tile has six neighbors: east, southeast, southwest, west, northwest, and northeast. These directions are given in your list, respectively, as e, se, sw, w, nw, and ne. A tile is identified by a series of these directions with no delimiters; for example, esenee identifies the tile you land on if you start at the reference tile and then move one tile east, one tile southeast, one tile northeast, and one tile east.
 
-Here's a more interesting example:
-```
-0: 4 1 5
-1: 2 3 | 3 2
-2: 4 4 | 5 5
-3: 4 5 | 5 4
-4: "a"
-5: "b"
-```
-Here, because rule 4 matches a and rule 5 matches b, rule 2 matches two letters that are the same (aa or bb), and rule 3 matches two letters that are different (ab or ba).
+Each time a tile is identified, it flips from white to black or from black to white. Tiles might be flipped more than once. For example, a line like esew flips a tile immediately adjacent to the reference tile, and a line like nwwswee flips the reference tile itself.
 
-Since rule 1 matches rules 2 and 3 once each in either order, it must match two pairs of letters, one pair with matching letters and one pair with different letters. This leaves eight possibilities: aaab, aaba, bbab, bbba, abaa, abbb, baaa, or babb.
+Here is a larger example:
 
-Rule 0, therefore, matches a (rule 4), then any of the eight options from rule 1, then b (rule 5): aaaabb, aaabab, abbabb, abbbab, aabaab, aabbbb, abaaab, or ababbb.
+sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew
+In the above example, 10 tiles are flipped once (to black), and 5 more are flipped twice (to black, then back to white). After all of these instructions have been followed, a total of 10 tiles are black.
 
-The received messages (the bottom part of your puzzle input) need to be checked against the rules so you can determine which are valid and which are corrupted. Including the rules and the messages together, this might look like:
-
-```
-0: 4 1 5
-1: 2 3 | 3 2
-2: 4 4 | 5 5
-3: 4 5 | 5 4
-4: "a"
-5: "b"
-```
-```
-ababbb
-bababa
-abbbab
-aaabbb
-aaaabbb
-```
-Your goal is to determine the number of messages that completely match rule 0. In the above example, ababbb and abbbab match, but bababa, aaabbb, and aaaabbb do not, producing the answer 2. The whole message must match all of rule 0; there can't be extra unmatched characters in the message. (For example, aaaabbb might appear to match rule 0 above, but it has an extra unmatched b on the end.)
-
-
+Go through the renovation crew's list and determine which tiles they need to flip. After all of the instructions have been followed, how many tiles are left with the black side up?
 
 ## Part Two 
 
